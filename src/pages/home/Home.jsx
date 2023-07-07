eed05a8f494a615aa0e45886e7147a7c112aa9cd
biduedson <112134265+biduedson@users.noreply.github.com>
2023-07-06 21:55:44 -0300

    configurando pastas e resets de css

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import './home.scss'
+function Home() {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Home
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'https://hamburgueriack.cyclic.app',
+        changeOrigin: true
+      },
+
+    }
+  }
+
 })
